eefa18a3dddb3e186c65f6f90d2a7facaf06e68a
Ingsay <ingsay177@gmail.com>
2023-11-18 13:24:43 +0100

    duzo_kodu

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+const restCountries = "https://restcountries.com/v3.1/name";
+
+export function fetchCountries(name) {
+    return fetch(`${restCountries}/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(`Failed to download country data for ${name}`);
+            }
+        })
+        .catch(error => {
+            throw new Error(error);
+        });
+}
